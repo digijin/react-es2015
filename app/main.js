9dc6906bda6c6cf10dc458861a1d714242092aa6
James Smith <james.smith@mi9.com.au>
2015-12-23 17:07:29 +1100

    init

---
+++ app/main.js
@@ -0,0 +1,6 @@
+'use strict';
+// var component = require('./component.js');
+import component from './component.js'
+
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+
+module.exports = {
+    entry: path.resolve(__dirname, 'app/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module:{
+      loaders:[
+        {
+          test: /\.js?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          query: {
+          }
+        }
+      ]
+    },
+    resolve:{
+      extensions: ['', '.js']
+    }
+};
